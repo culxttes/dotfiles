6b94d68982f8e95ab849b64bd90d21fbac598d27
Culottes <culottes@sagbot.com>
2024-07-26 02:18:01 +0200

    Change my OS from Fedora Sway to Arch hyprland

--- .zshrc
+++ .zshrc
@@ -1,27 +1,12 @@
-HISTFILE=$HOME/.zsh_history
-export HISTSIZE=1000000   # the number of items for the internal history list
-export SAVEHIST=1000000   # maximum number of items for the history file
+export ZSH="$HOME/.oh-my-zsh"
+plugins=(git)
 
-# The meaning of these options can be found in man page of `zshoptions`.
-setopt HIST_IGNORE_ALL_DUPS  # do not put duplicated command into history list
-setopt HIST_SAVE_NO_DUPS  # do not save duplicated command
-setopt HIST_REDUCE_BLANKS  # remove unnecessary blanks
-setopt INC_APPEND_HISTORY_TIME  # append command to history file immediately after execution
-setopt EXTENDED_HISTORY  # record command start time
-
-autoload -U up-line-or-beginning-search
-autoload -U down-line-or-beginning-search
-zle -N up-line-or-beginning-search
-zle -N down-line-or-beginning-search
-bindkey '^[[A' up-line-or-beginning-search
-bindkey '^[[B' down-line-or-beginning-search
-
-plugins=(git ssh-agent zsh-autosuggestions)
-
-eval $(/usr/bin/gnome-keyring-daemon)
-export SSH_AUTH_SOCK
+source $ZSH/oh-my-zsh.sh
 
 alias ssh="kitten ssh"
+alias icat="kitten icat"
+
+export SSH_AUTH_SOCK=$XDG_RUNTIME_DIR/gcr/ssh
 
 eval "$(oh-my-posh init zsh --config $HOME/.config/ohmyposh/zen.toml)"
 eval "$(zoxide init --cmd cd zsh)"
--- install.sh
+++ install.sh
@@ -1,48 +1,26 @@
 #!/bin/bash
 
-# Get the current directory
-current_dir=$(pwd)
-
-# Get the home directory of the user
-home_dir=$HOME
-
-# Directory for symbolic links for bin files
-bin_target_dir="/usr/local/bin"
-
-# Directory for symbolic links for config files
-config_target_dir="$home_dir/.config"
-
 # Create symbolic links for hidden files in the current directory
-for file in $current_dir/.[^.]*; do
+for file in $(pwd)/.[^.]*; do
   if [[ "$(basename "$file")" != .git* ]]; then
-    # Create a symbolic link in the home directory
-    ln -s "$file" "$home_dir"
+    ln -s "$file" "$HOME"
   fi
 done
 
+mkdir -p "$HOME/.config"
+
+ln -s "$(pwd)/logo.png" "$HOME/.config"
+ln -s "$(pwd)/background.jpeg" "$HOME/.config"
+
 # Create symbolic links for files and directories in the config directory
-if [ -d "$current_dir/config" ]; then
-  # Create the target .config directory if it doesn't exist
-  mkdir -p "$config_target_dir"
+if [ -d "$(pwd)/config" ]; then
 
-  for file in $current_dir/config/*; do
-    # Create a symbolic link in ~/.config
-    ln -s "$file" "$config_target_dir"
+  for file in $(pwd)/config/*; do
+    ln -s "$file" "$HOME/.config"
   done
 else
-  echo "No config directory found in $current_dir"
+  echo "No config directory found in $(pwd)"
 fi
 
-# Create symbolic links for files in the bin directory
-if [ -d "$current_dir/bin" ]; then
-  for file in $current_dir/bin/*; do
-    if [ -f "$file" ]; then
-      # Create a symbolic link in /usr/local/bin
-      sudo ln -s "$file" "$bin_target_dir"
-    fi
-  done
-else
-  echo "No bin directory found in $current_dir"
-fi
 
 echo "All symbolic links created."
