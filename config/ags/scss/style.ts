ed3228ba07d605610c40c746ab205dde2be3d082
Culottes <culottes@sagbot.com>
2024-10-31 16:10:19 +0100

    update ags

--- config/ags/scss/style.ts
+++ config/ags/scss/style.ts
@@ -1,74 +1,79 @@
-/* eslint-disable max-len */
-import { type Opt } from "lib/option"
-import options from "options"
-import { bash, dependencies } from "lib/utils"
+import options from 'options';
+import { bash, dependencies } from 'lib/utils';
+import { MatugenColors, RecursiveOptionsObject } from 'lib/types/options';
+import { initializeTrackers } from './optionsTrackers';
+import { generateMatugenColors, replaceHexValues } from '../services/matugen/index';
+import { isHexColor } from 'globals/variables';
 
-const deps = [
-    "font",
-    "theme",
-    "bar.flatButtons",
-    "bar.position",
-    "bar.battery.charging",
-    "bar.battery.blocks",
-]
+const deps = ['font', 'theme', 'bar.flatButtons', 'bar.position', 'bar.battery.charging', 'bar.battery.blocks'];
 
-const $ = (name: string, value: string | Opt<any>) => `$${name}: ${value};`
+function extractVariables(theme: RecursiveOptionsObject, prefix = '', matugenColors?: MatugenColors): string[] {
+    let result = [] as string[];
+    for (const key in theme) {
+        if (!theme.hasOwnProperty(key)) {
+            continue;
+        }
 
-function extractVariables(theme: any, prefix: string = ""): string[] {
-    let result: string[] = [];
-    for (let key in theme) {
-        if (theme.hasOwnProperty(key)) {
-            const value = theme[key];
-            const newPrefix = prefix ? `${prefix}-${key}` : key;
+        const themeValue = theme[key];
 
-            if (typeof value === 'object' && value !== null && !Array.isArray(value)) {
-                // Check if the object contains an Opt value or is a nested object
-                if (typeof value.value !== 'undefined') {
-                    result.push($(`${newPrefix}`, `${value.value}`));
-                } else {
-                    result = result.concat(extractVariables(value, newPrefix));
-                }
-            } else if (typeof value === 'function' && value.name === 'opt') {
-                result.push($(`${newPrefix}`, value));
-            }
+        const newPrefix = prefix ? `${prefix}-${key}` : key;
+
+        const replacedValue =
+            isHexColor(themeValue.value) && matugenColors !== undefined
+                ? replaceHexValues(themeValue.value, matugenColors)
+                : themeValue.value;
+
+        if (typeof themeValue === 'function') {
+            result.push(`$${newPrefix}: ${replacedValue};`);
+            continue;
+        }
+        if (typeof themeValue !== 'object' || themeValue === null || Array.isArray(themeValue)) continue;
+
+        if (typeof themeValue.value !== 'undefined') {
+            result.push(`$${newPrefix}: ${replacedValue};`);
+        } else {
+            result = result.concat(extractVariables(themeValue, newPrefix, matugenColors));
         }
     }
+
     return result;
 }
 
-const variables = () => [
-    ...extractVariables(options.theme),
-];
-
-async function resetCss() {
-    if (!dependencies("sass"))
-        return
+const resetCss = async (): Promise<void> => {
+    if (!dependencies('sass')) return;
 
     try {
-        const vars = `${App.configDir}/scss/variables.scss`
-        const css = `${TMP}/main.css`
-        const scss = `${App.configDir}/scss/entry.scss`
+        const matugenColors = await generateMatugenColors();
+
+        const variables = extractVariables(options.theme as RecursiveOptionsObject, '', matugenColors);
+
+        const vars = `${TMP}/variables.scss`;
+        const css = `${TMP}/main.css`;
+        const scss = `${TMP}/entry.scss`;
         const localScss = `${App.configDir}/scss/main.scss`;
 
-        const imports = [vars].map(f => `@import '${f}';`)
+        const themeVariables = variables;
+        const integratedVariables = themeVariables;
 
-        await Utils.writeFile(variables().join("\n"), vars)
+        const imports = [vars].map((f) => `@import '${f}';`);
+
+        await Utils.writeFile(integratedVariables.join('\n'), vars);
 
         let mainScss = Utils.readFile(localScss);
         mainScss = `${imports}\n${mainScss}`;
 
-        await Utils.writeFile(mainScss, scss)
+        await Utils.writeFile(mainScss, scss);
 
-        await bash(`sass ${scss} ${css}`);
+        await bash(`sass --load-path=${App.configDir}/scss/ ${scss} ${css}`);
 
         App.applyCss(css, true);
     } catch (error) {
-        error instanceof Error
-            ? logError(error)
-            : console.error(error)
+        console.error(error);
     }
-}
+};
 
-Utils.monitorFile(`${App.configDir}/scss/style`, resetCss)
-options.handler(deps, resetCss)
-await resetCss()
+initializeTrackers(resetCss);
+
+Utils.monitorFile(`${App.configDir}/scss/style`, resetCss);
+options.handler(deps, resetCss);
+await resetCss();
