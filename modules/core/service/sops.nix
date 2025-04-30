75dca96624f9cd85eb372c42406eb721f676b553
Culottes <culottes@sagbot.com>
2025-04-30 14:16:01 +0200

    fix(sops.nix): add comment to explain ssh-to-age command usage

--- modules/core/service/sops.nix
+++ modules/core/service/sops.nix
@@ -7,7 +7,8 @@
 
   sops.defaultSopsFile = ../../../secrets/secrets.yaml;
   sops.defaultSopsFormat = "yaml";
-  
+
+  # nix run nixpkgs#ssh-to-age -- -private-key -i ~/.ssh/nixos > ~/.config/sops/age/keys.txt
   sops.age.keyFile = "/home/${username}/.config/sops/age/keys.txt";
 
   sops.secrets."language-tool/username" = {
