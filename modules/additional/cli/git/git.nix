1722629073b3ff73267aaef2b4732d5c91aeb478
Culottes <culottes@sagbot.com>
2025-05-16 11:45:47 +0200

    fix(git identity): Change git identity to Culottes

--- modules/additional/cli/git/git.nix
+++ modules/additional/cli/git/git.nix
@@ -4,8 +4,8 @@
   home-manager.users.${username} = {
     programs.git = {
       enable = true;
-      userName = "Lurgrid";
-      userEmail = "lurgrid@sagbot.com";
+      userName = "Culottes";
+      userEmail = "culottes@sagbot.com";
       aliases = {
         pull-all = "!git pull && git submodule update --init --recursive";
       };
