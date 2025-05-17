2c008142c0690d881dffece0b7f7379ad5fc22fe
Culottes <culottes@sagbot.com>
2025-05-17 17:15:09 +0200

    fix(hardware-specific): corrected accidental exchange of two files

--- hosts/tantale/hardware-specific.nix
+++ hosts/tantale/hardware-specific.nix
@@ -1,12 +1,4 @@
 { pkgs, ... }:
 
 {
-  boot.swraid = {
-    enable = true;
-    mdadmConf = ''
-      ARRAY /dev/md0 metadata=1.2 UUID=940fbd14:461cea16:c1c25c6f:67a97efc
-      ARRAY /dev/md1 metadata=1.2 UUID=ada8290d:e8e0a062:33381635:1ab2b4fe
-      MAILADDR mdadm-error@sagbot.com
-    '';
-  };
 }
