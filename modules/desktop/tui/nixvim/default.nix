0ee011fa5ada6f479b9d06b5e707854f4b0a31fc
Culottes <culottes@sagbot.com>
2025-05-19 08:37:58 +0200

    feat(modules/desktop/gui/graphics/feh): add feh graphic tool

--- modules/desktop/tui/nixvim/default.nix
+++ modules/desktop/tui/nixvim/default.nix
@@ -1,5 +1,23 @@
+{ pkgs, username, ... }:
+
 {
   imports = [
     ./plugins
   ];
+
+  home-manager.users.${username}.programs.nixvim = {
+    extraPackages = with pkgs; [
+      feh
+    ];
+
+    extraConfigLua = ''
+      vim.api.nvim_create_autocmd("BufReadPre", {
+        pattern = {"*.png", "*.jpg", "*.jpeg", "*.gif", "*.bmp", "*.webp"},
+        callback = function(args)
+          local file = args.file
+          vim.fn.jobstart({ "feh", file }, { detach = true })
+        end,
+      })
+    '';
+  };
 }
