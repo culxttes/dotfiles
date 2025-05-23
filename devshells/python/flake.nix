c26a7a10e0c0773998fce7ba117d0661238cece7
Culottes <culottes@sagbot.com>
2025-05-23 08:47:18 +0200

    feat(devshells/python): add devshells template for python

---
+++ devshells/python/flake.nix
@@ -0,0 +1,42 @@
+{
+  description = "Python devshell with requests";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+      ...
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+        };
+
+        # nix run github:nix-community/pip2nix -- generate -r requirements.txt
+        # nix run github:nix-community/pip2nix -- generate panda
+        packageOverrides = pkgs.callPackage ./python-packages.nix { };
+        python = pkgs.python3.override {
+          inherit packageOverrides;
+        };
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          packages = [
+            (python.withPackages (
+              ps: with ps; [
+                # packages
+              ]
+            ))
+          ];
+        };
+      }
+    );
+}
