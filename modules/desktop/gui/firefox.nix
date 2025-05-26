6bf6e1fe4abee17e28223187bc9628aa954da018
Culottes <culottes@sagbot.com>
2025-05-26 07:59:50 +0200

    fix(firefox): use programs.firefox

--- modules/desktop/gui/firefox.nix
+++ modules/desktop/gui/firefox.nix
@@ -7,9 +7,10 @@
 
 {
   home-manager.users.${username} = {
-    home.packages = [
-      firefox.packages.${pkgs.system}.firefox-nightly-bin
-    ];
+    programs.firefox = {
+      enable = true;
+      package = firefox.packages.${pkgs.system}.firefox-nightly-bin;
+    };
 
     home.sessionVariables = {
       MOZ_ENABLE_WAYLAND = "1";
