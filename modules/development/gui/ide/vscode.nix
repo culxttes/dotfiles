8b2f8d1e641b617ea637948a801896e65e06bacc
Culottes <culottes@sagbot.com>
2025-06-02 06:30:44 +0000

    chore: auto-update scripts output

--- modules/development/gui/ide/vscode.nix
+++ modules/development/gui/ide/vscode.nix
@@ -7,7 +7,7 @@
         src = (
           builtins.fetchTarball {
             url = "https://code.visualstudio.com/sha/download?build=insider&os=linux-x64";
-            sha256 = "1kbidggnrrly2dij4g4ny2pza2pcnv82jy7bzr5398y9xsxcq0nq";
+            sha256 = "0cnidqzh8j484kbnxbh4fgrjx2zl7wd2lfwyx9a4rhwkvrlhisx5";
           }
         );
         version = "latest";
