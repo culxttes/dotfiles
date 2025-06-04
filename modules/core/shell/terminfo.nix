02b1b7692cd69bcee47e755951062e92d614f763
Culottes <culottes@sagbot.com>
2025-06-04 12:16:32 +0200

    chore(core/shell/terminfo): add comment

--- modules/core/shell/terminfo.nix
+++ modules/core/shell/terminfo.nix
@@ -1,5 +1,12 @@
 { pkgs, ... }:
+/*
+  terminfo definitions for :
+    - the Kitty terminal.
 
+  This ensures proper terminal capabilities (like colors and key handling) are
+  recognized by applications, especially when connecting to remote systems or
+  using tools that rely on accurate terminfo entries.
+*/
 {
   environment.systemPackages = [
     pkgs.kitty.terminfo
