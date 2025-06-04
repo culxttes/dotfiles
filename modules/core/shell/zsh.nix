9ec9d0ca4e43b346993a025e2c91b7cd4b1c2fc0
Culottes <culottes@sagbot.com>
2025-06-04 16:06:11 +0200

    fix(core/shell/zsh): remove --upgrade

--- modules/core/shell/zsh.nix
+++ modules/core/shell/zsh.nix
@@ -28,7 +28,7 @@
           sudo NIX_CONFIG="access-tokens = github.com=$(sudo cat ${
             config.sops.secrets."github/token/readonly".path
           })" nix flake update --flake ~/git/dotfiles/ && \
-          sudo nixos-rebuild switch --upgrade --flake ~/git/dotfiles/
+          sudo nixos-rebuild switch --flake ~/git/dotfiles/
         '';
       };
       initContent = ''
