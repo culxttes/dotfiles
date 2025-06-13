e8df06a86e08eca33720bb0a56ad928ecb0ffdcf
Culottes <culottes@sagbot.com>
2025-06-13 16:25:33 +0000

    chore: auto-update scripts output

--- modules/desktop/gui/spotify/spotx.nix
+++ modules/desktop/gui/spotify/spotx.nix
@@ -2,7 +2,7 @@ final: prev:
 let
   spotx = prev.fetchurl {
     url = "https://raw.githubusercontent.com/SpotX-Official/SpotX-Bash/refs/heads/main/spotx.sh";
-    hash = "sha256-/p6cJKzaZzjcLJISFudstQjs+lPXnXx4f0vxKbF9Sqw=";
+    hash = "sha256-XFPSzcgMJFruFKm0P4TejMMq/Gg5lXU8jY9EJtXXZeo=";
   };
 in
 {
