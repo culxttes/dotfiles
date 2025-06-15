37af6772eea40cbf0bedc9c35533eb9d7ace1263
Culottes <culottes@sagbot.com>
2025-06-15 11:01:35 +0200

    feat(core/nix/settings): use cachix of nix-community

--- modules/core/nix/settings.nix
+++ modules/core/nix/settings.nix
@@ -6,10 +6,12 @@
     substituters = [
       "https://hyprland.cachix.org"
       "https://focal.cachix.org"
+      "https://nix-community.cachix.org"
     ];
     trusted-public-keys = [
       "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
       "focal.cachix.org-1:/YkOWkXNH2uK7TnskrVMvda8LyCe4iIbMM1sZN2AOXY="
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
     ];
     experimental-features = [
       "nix-command"
