dee32aeb217d68e26778ba4ec37c4dbf80922c7f
Culottes <culottes@sagbot.com>
2025-07-18 12:11:54 +0200

    feat(core/nix/settings): remove dirty warning

--- modules/core/nix/settings.nix
+++ modules/core/nix/settings.nix
@@ -16,10 +16,13 @@
       "focal.cachix.org-1:/YkOWkXNH2uK7TnskrVMvda8LyCe4iIbMM1sZN2AOXY="
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
     ];
+
     experimental-features = [
       "nix-command"
       "flakes"
     ];
+
+    warn-dirty = false;
 
     download-buffer-size = 1073741824;
   };
