badd2f32438924be995f10d38a962f52c8d2a9e8
Culottes <culottes@sagbot.com>
2025-09-09 15:50:48 +0200

    feat(rebuild): generate rebuild command for each host in group server

--- modules/core/programs/rebuild.nix
+++ modules/core/programs/rebuild.nix
@@ -1,39 +1,55 @@
-{ pkgs, username, ... }:
+{
+  pkgs,
+  lib,
+  username,
+  systemInfo,
+  ...
+}:
 
+let
+  serverHosts =
+    let
+      names = builtins.attrNames systemInfo;
+    in
+    builtins.filter (name: builtins.elem "server" systemInfo.${name}.groups) names;
+
+  genCase = name: ''
+    ${name} )
+      nixos-rebuild switch \
+        --flake ~/git/dotfiles#${name} \
+        --build-host ${username}@${name}.sagbot.com \
+        --target-host ${username}@${name}.sagbot.com \
+        --sudo \
+        --ask-sudo-password
+      ;;
+  '';
+in
 {
   home-manager.users.${username} = {
     home.packages = [
-      (pkgs.writeShellScriptBin "rebuild" ''
-        #!/usr/bin/env bash
-
-        set -euo pipefail
-
-        case "''\${1-}" in
-          "" )
-            nixos-rebuild switch \
-              --flake ~/git/dotfiles/ \
-              --sudo
-            ;;
-          remote )
-            nixos-rebuild switch \
-              --flake ~/git/dotfiles/ \
-              --build-host culottes@sisyphe.sagbot.com \
-              --sudo
-            ;;
-          sisyphe )
-            nixos-rebuild switch \
-              --flake ~/git/dotfiles#sisyphe \
-              --build-host culottes@sisyphe.sagbot.com \
-              --target-host culottes@sisyphe.sagbot.com \
-              --sudo \
-              --ask-sudo-password
-            ;;
-          * )
-            echo "Usage: $0 [remote|sisyphe]"
-            exit 1
-            ;;
-        esac
-      '')
+      (pkgs.writeShellApplication {
+        name = "rebuild";
+        text = ''
+          case "''\${1-}" in
+            "" )
+              nixos-rebuild switch \
+                --flake ~/git/dotfiles/ \
+                --sudo
+              ;;
+            remote )
+              nixos-rebuild switch \
+                --flake ~/git/dotfiles/ \
+                --build-host ${username}@sisyphe.sagbot.com \
+                --sudo
+              ;;
+            ${lib.concatStringsSep "" (map genCase serverHosts)}
+            * )
+              echo "Usage: $0 [remote|${lib.concatStringsSep "|" serverHosts}]"
+              exit 1
+              ;;
+          esac
+        '';
+      })
     ];
   };
 }
