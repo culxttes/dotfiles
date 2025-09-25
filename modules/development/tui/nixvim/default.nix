62321eaa9667a061207c226342584dc627807527
Culottes <culottes@sagbot.com>
2025-09-25 08:18:14 +0200

    feat(development/nixvim): open image with feh and pdf with zathura but don't open the buffer

--- modules/development/tui/nixvim/default.nix
+++ modules/development/tui/nixvim/default.nix
@@ -15,8 +15,22 @@
       vim.api.nvim_create_autocmd("BufReadPre", {
         pattern = {"*.png", "*.jpg", "*.jpeg", "*.gif", "*.bmp", "*.webp"},
         callback = function(args)
-          local file = args.file
-          vim.fn.jobstart({ "feh", file }, { detach = true })
+          vim.fn.jobstart({ "feh", args.file }, { detach = true })
+
+          vim.schedule(function()
+            vim.api.nvim_buf_delete(args.buf, { force = true })
+          end)
+        end,
+      })
+
+      vim.api.nvim_create_autocmd("BufReadPre", {
+        pattern = {"*.pdf"},
+        callback = function(args)
+          vim.fn.jobstart({ "zathura", args.file }, { detach = true })
+
+          vim.schedule(function()
+            vim.api.nvim_buf_delete(args.buf, { force = true })
+          end)
         end,
       })
     '';
