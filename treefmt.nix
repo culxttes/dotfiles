d4ad99f4da9de3b5de312dec6e873f6a21421956
Culottes <culottes@sagbot.com>
2025-09-29 14:17:14 +0200

    feat: remove programs.nixfmt.package

--- treefmt.nix
+++ treefmt.nix
@@ -5,17 +5,12 @@
 
   settings.verbose = 1;
 
-  programs.keep-sorted = {
-    enable = true;
-  };
-
-  programs.nixfmt = {
-    enable = true;
-    package = pkgs.nixfmt;
-  };
+  programs.keep-sorted.enable = true;
+  programs.nixfmt.enable = true;
 
   programs.yamlfmt = {
     enable = true;
+
     settings.formatter = {
       retain_line_breaks = true;
     };
