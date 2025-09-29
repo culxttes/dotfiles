4c0e1ca5f9c209c4bfdaa26e2636fd52689f3d75
Culottes <culottes@sagbot.com>
2025-09-29 14:06:21 +0200

    feat: add formatter for yaml file

--- treefmt.nix
+++ treefmt.nix
@@ -5,5 +5,19 @@
 
   settings.verbose = 1;
 
-  programs.nixfmt.enable = true;
+  programs.keep-sorted = {
+    enable = true;
+  };
+
+  programs.nixfmt = {
+    enable = true;
+    package = pkgs.nixfmt;
+  };
+
+  programs.yamlfmt = {
+    enable = true;
+    settings.formatter = {
+      retain_line_breaks = true;
+    };
+  };
 }
