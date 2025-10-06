a0145336b2d2bee9d05883aee37b34f974ac739e
Culottes <culottes@sagbot.com>
2025-10-06 07:32:03 +0200

    feat: start nvf config

--- modules/additional/tui/nvf/default.nix
+++ modules/additional/tui/nvf/default.nix
@@ -7,5 +7,21 @@
       nvf.homeManagerModules.default
       # keep-sorted end
     ];
+
+    programs.nvf = {
+      enable = false;
+
+      enableManpages = true;
+
+      settings = {
+        vim.viAlias = false;
+        vim.vimAlias = true;
+        vim.lsp = {
+          enable = true;
+          formatOnSave = false;
+          lspconfig.enable = true;
+        };
+      };
+    };
   };
 }
