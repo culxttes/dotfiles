d0c1a9ef8bb13cbb4bb93edb5105803b79a33998
Culottes <culottes@sagbot.com>
2025-10-06 17:33:11 +0200

    feat: add update inputs specify

--- modules/core/programs/update/default.nix
+++ modules/core/programs/update/default.nix
@@ -20,17 +20,13 @@
         set -euo pipefail
 
         case "''\${1-}" in
-          "" )
-            NIX_CONFIG="access-tokens = github.com=$(cat ${
-              config.sops.secrets."github/token/readonly".path
-            })" nix flake update --flake ~/git/dotfiles/
-            ;;
           commit )
             git commit -m "chore(flake.lock): update dependencies" -n
             ;;
           * )
-            echo "Usage: $0 [commit]"
-            exit 1
+            NIX_CONFIG="access-tokens = github.com=$(cat ${
+              config.sops.secrets."github/token/readonly".path
+            })" nix flake update --flake ~/git/dotfiles/ "$@"
             ;;
         esac
       '')
