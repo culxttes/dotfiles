141d822c71898858f8759e08d371f82b5ef99edf
Culottes <culottes@sagbot.com>
2025-10-17 08:36:01 +0200

    feat: remove dependency nixvim

--- flake.nix
+++ flake.nix
@@ -40,10 +40,6 @@
       # url = "git+file:///home/culottes/git/nixpkgs/";
       url = "github:nixos/nixpkgs";
     };
-    nixvim = {
-      url = "github:nix-community/nixvim";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
     noctalia = {
       url = "github:noctalia-dev/noctalia-shell";
       inputs.nixpkgs.follows = "nixpkgs";
