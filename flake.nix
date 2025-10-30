64a383441c587d2e66aba793d401db5a88f62b43
c2fc2f <contact@c2fc2f.com>
2025-10-30 19:41:26 +0100

    chore(flake.lock): update dependencies

--- flake.nix
+++ flake.nix
@@ -41,7 +41,8 @@
       url = "github:nixos/nixpkgs";
     };
     noctalia = {
-      url = "github:noctalia-dev/noctalia-shell";
+      url = "git+file:///home/culottes/git/noctalia-shell/";
+      # url = "github:noctalia-dev/noctalia-shell";
       inputs.nixpkgs.follows = "nixpkgs";
       inputs.quickshell.follows = "quickshell";
     };
