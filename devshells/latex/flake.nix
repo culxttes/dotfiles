e49404b97c89eaed42e20d6355a40768d3002cbd
c2fc2f <contact@c2fc2f.com>
2025-11-04 07:55:22 +0100

    feat(devshells): add a devshells for LaTeX

---
+++ devshells/latex/flake.nix
@@ -0,0 +1,31 @@
+{
+  description = "LaTeX development environment";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs";
+    flake-utils = {
+      url = "github:numtide/flake-utils";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs =
+    {
+      nixpkgs,
+      flake-utils,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            texlive.combined.scheme-full
+            tex-fmt
+          ];
+        };
+      }
+    );
+}
