640c56cb2bf4fcbb12f99a4f0d5217192d72343c
c2fc2f <contact@c2fc2f.com>
2025-11-04 09:55:29 +0100

    feat(ollama): remove ollama-webui

--- modules/sisyphe/services/ollama/default.nix
+++ modules/sisyphe/services/ollama/default.nix
@@ -1,17 +1,19 @@
 {
   services.ollama = {
     enable = true;
+
+    host = "127.0.0.1";
+    port = 11434;
+
+    environmentVariables = {
+      OLLAMA_LLM_LIBRARY = "cpu";
+    };
+
     loadModels = [
       "qwen3:8b"
       "mistral:7b"
       "embeddinggemma:latest"
       "qwen3-embedding:latest"
     ];
-  };
-
-  services.open-webui = {
-    enable = true;
-    port = 3380;
-    host = "127.0.0.208";
   };
 }
