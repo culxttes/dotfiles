2cdd2ba022ee4c75338ca4a7781ba45b776d9e22
c2fc2f <contact@c2fc2f.com>
2025-11-08 10:57:59 +0100

    feat: auto startup streamcontroller

--- modules/niobe/programs/streamdeck/streamcontroller.nix
+++ modules/niobe/programs/streamdeck/streamcontroller.nix
@@ -5,5 +5,26 @@
     home.packages = [
       pkgs.streamcontroller
     ];
+
+    systemd.user.services.streamcontroller = {
+      Unit = {
+        Description = "Stream Controller";
+        After = [
+          "graphical-session.target"
+        ];
+      };
+
+      Service = {
+        ExecStart = "${pkgs.streamcontroller}/bin/streamcontroller -b";
+        Restart = "on-failure";
+        RestartSec = 5;
+      };
+
+      Install = {
+        WantedBy = [
+          "graphical-session.target"
+        ];
+      };
+    };
   };
 }
