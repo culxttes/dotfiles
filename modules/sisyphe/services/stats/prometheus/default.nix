8d9da841e2c143193e783959858a8744cb02abd9
c2fc2f <contact@c2fc2f.com>
2025-11-14 14:24:37 +0100

    feat(prometheus): add node exporter

--- modules/sisyphe/services/stats/prometheus/default.nix
+++ modules/sisyphe/services/stats/prometheus/default.nix
@@ -2,10 +2,37 @@ let
   addr = "127.0.0.84:8404";
 in
 {
-  services.prometheus = {
+  services.prometheus = rec {
     enable = true;
 
+    exporters = {
+      node = {
+        enable = true;
+
+        listenAddress = "127.0.0.1";
+        port = 9000;
+
+        enabledCollectors = [
+          "systemd"
+        ];
+        extraFlags = [
+          "--collector.ethtool"
+          "--collector.tcpstat"
+        ];
+      };
+    };
+
     scrapeConfigs = [
+      {
+        job_name = "node";
+        static_configs = [
+          {
+            targets = [
+              "${exporters.node.listenAddress}:${toString exporters.node.port}"
+            ];
+          }
+        ];
+      }
       {
         job_name = "haproxy";
         static_configs = [
