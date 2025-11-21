9d859c43f0778f34094d94c39c515af4a8820179
c2fc2f <contact@c2fc2f.com>
2025-11-21 08:43:13 +0100

    feat(core/makeIso): don't use builders by default but pass all the reminding params

--- modules/core/programs/makeIso/default.nix
+++ modules/core/programs/makeIso/default.nix
@@ -13,10 +13,8 @@ let
 
   genCase = name: ''
     ${name} )
-      nix build \
-        --builders 'ssh-ng://${username}@sisyphe.sagbot.com x86_64-linux ~/.ssh/${username} 4 1 benchmark,big-parallel,kvm,nixos-test' \
-        --max-jobs 0 \
-        ~/git/dotfiles#nixosConfigurations.${name}.config.system.build.isoImage       ;;
+      nix build ~/git/dotfiles#nixosConfigurations.${name}.config.system.build.isoImage "''${@:2}"
+      ;;
   '';
 in
 {
@@ -25,8 +23,8 @@ in
       (pkgs.writeShellApplication {
         name = "makeIso";
         text = ''
-          case "''\${1-}" in
-            ${concatStringsSep "" (map genCase isoHosts)}
+          case "''${1-}" in
+          ${pkgs.lib.custom.indent 2 (concatStringsSep "" (map genCase isoHosts))}
             * )
               echo "Usage: $0 [${concatStringsSep "|" isoHosts}]"
               exit 1
