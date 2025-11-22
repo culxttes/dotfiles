9d97b90d5088445e926be59328d2dcdb21382a81
c2fc2f <contact@c2fc2f.com>
2025-11-22 10:15:59 +0100

    fix(workstation/pipewire): remove AGC

--- modules/workstation/services/pipewire/default.nix
+++ modules/workstation/services/pipewire/default.nix
@@ -6,26 +6,15 @@
 
     alsa.enable = true;
     pulse.enable = true;
+    wireplumber.enable = true;
 
-    wireplumber = {
-      enable = true;
-
-      extraConfig = {
-        "99-disable-agc.conf" = {
-          "monitor.alsa.rules" = [
-            {
-              "matches" = [
-                {
-                  "node.name" = "~alsa_input.*";
-                }
-              ];
-              "actions" = {
-                "update-props" = {
-                  "audio.webrtc.auto-gain-control" = false;
-                };
-              };
-            }
-          ];
+    extraConfig.pipewire = {
+      "10-disable-volume-limiting" = {
+        "context.properties" = {
+          "default.clock.rate" = 48000;
+        };
+        "stream.properties" = {
+          "channelmix.normalize" = false;
         };
       };
     };
